d75da46766fa3f275fd112d854d19779641555c9
Justin Hollister <hollisterjustin2025@serrahs.com>
2024-10-05 12:23:37 -0700

    unused imports

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -1,9 +1,7 @@
 package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Volts;
-import static frc.robot.constants.ShooterConstants.tangentialNoteFlightTime;
 
-import java.lang.reflect.Field;
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 
@@ -29,7 +27,6 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
