ba83fe1f7051e950b7f06f1a40bc6ea2ba0f616d
Justin Hollister <hollisterjustin2025@serrahs.com>
2024-10-08 20:52:32 -0700

    got rid of distance sensor in intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.Subsystems;
+package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -11,24 +11,16 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.DistanceSensorReader;
-import frc.robot.Constants.IntakeConstants;
-import frc.robot.Constants.ShooterConstants;
+import frc.robot.constants.IntakeConstants;
+import frc.robot.constants.ShooterConstants;
 
 public class Intake extends SubsystemBase {
   private TalonFX intakeMotor = new TalonFX(IntakeConstants.intakeMotorId, IntakeConstants.canbus);
   private TalonFX feedMotor = new TalonFX(IntakeConstants.feedMotorId, ShooterConstants.canbus);
-  private DistanceSensorReader rangeSensorReader = new DistanceSensorReader();
 
-  /*Run on seperate threat to avoid loop overruns */
-  private Notifier rangeSensorNotifier = new Notifier(rangeSensorReader);
 
   /** Creates a new Intake. */
   public Intake() {
-    rangeSensorReader.run();
-    rangeSensorNotifier.setName("Range Sensors");
-    rangeSensorNotifier.startPeriodic(0.01); //Run it at a higher freq to hopefully help intakes
-
     feedMotor.getConfigurator().apply(
       new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake));
     intakeMotor.getConfigurator().apply(
@@ -81,9 +73,5 @@ public class Intake extends SubsystemBase {
 
   public Command runBack(){
     return this.run(() -> feedMotor.set(-0.1));
-  }
-
-  public boolean noteDetected(){
-    return rangeSensorReader.getRange() < IntakeConstants.rangeSensorThreshold;
   }
 }
